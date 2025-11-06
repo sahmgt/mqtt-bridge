30dd2a5b4b8ac15135fd4e3a4002114e24ac11b9
sahmgt <109300849+sahmgt@users.noreply.github.com>
2025-11-06 06:14:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "🚀 Запуск Mosquitto..."
 mosquitto -c /etc/mosquitto/mosquitto.conf &
-sleep 3  # трохи зачекати, поки брокер підніметься
+sleep 3  # зачекай поки брокер стартує
 
 echo "⚙️  Запуск bridge.py..."
 python3 /app/bridge.py &
+sleep 3  # дати йому стабілізуватись
 
-sleep 3  # ще трохи для стабільності
 echo "🌐 Запуск Cloudflare Tunnel..."
-cloudflared tunnel run --token $TUNNEL_TOKEN
+cloudflared tunnel --url http://localhost:10000 --protocol http2
