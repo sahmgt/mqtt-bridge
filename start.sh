17aacc863738c75c8054400c7a24b20f6f36fee7
sahmgt <109300849+sahmgt@users.noreply.github.com>
2025-11-06 05:20:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Запуск Mosquitto..."
+echo "🚀 Запуск Mosquitto..."
 mosquitto -c /etc/mosquitto/mosquitto.conf &
+sleep 3  # трохи зачекати, поки брокер підніметься
 
-echo "Запуск bridge.py..."
+echo "⚙️  Запуск bridge.py..."
 python3 /app/bridge.py &
 
-echo "Очікування 3 секунди для стабілізації..."
-sleep 3
-
-echo "Запуск Cloudflare Tunnel..."
+sleep 3  # ще трохи для стабільності
+echo "🌐 Запуск Cloudflare Tunnel..."
 cloudflared tunnel --url http://localhost:10000
+
