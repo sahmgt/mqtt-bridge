8a236b8ba9239a84d23623c16b97898a89565767
sahmgt <109300849+sahmgt@users.noreply.github.com>
2025-11-06 04:28:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# Запускаємо healthcheck одразу
-python3 /app/healthcheck.py &
-echo "Healthcheck HTTP running on port 10000"
-
-# Чекаємо 10 секунд — Render визначить вебпорт
-sleep 10
-
-# Тепер запускаємо Mosquitto
+# Запускаємо Mosquitto через WebSocket
 mosquitto -c /etc/mosquitto/mosquitto.conf &
-echo "Mosquitto started on port 1883"
+echo "🌀 Mosquitto WebSocket на порті 10000"
 
-# Ще трохи пауза для стабільності
+# Коротка затримка для стабільності
 sleep 3
 
 # Запускаємо bridge
