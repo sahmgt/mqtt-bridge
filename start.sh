6bc3baa8b19c2a44f65416e5cea27bacafe3d127
sahmgt <109300849+sahmgt@users.noreply.github.com>
2025-11-06 04:02:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-mosquitto -c /etc/mosquitto/mosquitto.conf &
-sleep 3
+# Спочатку запускаємо healthcheck-сервер
 python3 /app/healthcheck.py &
+
+# Даємо Render'у побачити HTTP порт 10000
+sleep 5
+
+# Тепер запускаємо MQTT брокер
+mosquitto -c /etc/mosquitto/mosquitto.conf &
+
+# І трохи пізніше сам bridge
+sleep 3
 python3 /app/bridge.py
